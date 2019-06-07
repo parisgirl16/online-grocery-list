75f41c560e7c029d6cf8f8914011dc99892b5e92
parisgirl16 <40363354+parisgirl16@users.noreply.github.com>
2019-06-07 16:54:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var submitBtn = $('.submit-btn');
+var cardContainer = $('.card-container');
+
+submitBtn.on("click", doAppend);
+
+function doAppend() {
+  var groceryInput = $('.grocery-input').val();
+  cardContainer.append(`<div class="name-card"><input type="checkbox" class="checkbox">${groceryInput}</div>`);
+  $('.grocery-input').val("")
+
+var checkbox = $('.checkbox');
+checkbox.on("click", remove);
+
+  storePlan(cardContainer);
+};
+
+function remove(){
+  event.target.parentNode.remove();
+}
+
+function storePlan(e) {
+  e.preventDefault();
+  localStorage.setItem('name-card', name-card);
+}
