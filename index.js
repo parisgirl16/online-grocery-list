e9789937460b756296f401e7e87289016c060fc8
parisgirl16 <40363354+parisgirl16@users.noreply.github.com>
2019-06-07 19:53:03 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -23,29 +23,5 @@ function storePlan(e) {
   localStorage.setItem('name-card', name-card);
 }
 
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class SharingService {
-  private storageName: string = "Settings";
-
-  constructor() { }
-
-  setSettings(data: any) {
-    localStorage.setItem(this.storageName, JSON.stringify(data));
-  }
-
-  getUserSettings() {
-    let data = localStorage.getItem(this.storageName);
-    return JSON.parse(data);
-  }
-
-  clearUserSettings() {
-    localStorage.removeItem(this.storageName);
-  }
-
-  cleanAll() {
-    localStorage.clear()
-  }
-
-}
+localStorage.setItem('name-card', name-card);
+document.getElementByClass("").innerHTML = localStorage.getItem('name-card');
