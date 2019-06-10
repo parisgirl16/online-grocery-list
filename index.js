411e1ab2fcb4d386a39ba326bc8d6515eb504b62
parisgirl16 <40363354+parisgirl16@users.noreply.github.com>
2019-06-10 11:15:17 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var changeBtn = $('.change-btn');
+var header = $('h3');
+
+changeBtn.on("click", changeStore)
+
+function changeStore() {
+  var storeName = $('.store-name').val();
+  header.text(`${storeName} List`);
+  $('.store-name').val("");
+}
+
 var submitBtn = $('.submit-btn');
 var cardContainer = $('.card-container');
 
@@ -6,13 +17,13 @@ submitBtn.on("click", doAppend);
 function doAppend() {
   var groceryInput = $('.grocery-input').val();
   cardContainer.append(`<div class="name-card"><input type="checkbox" class="checkbox">${groceryInput}</div>`);
-  $('.grocery-input').val("")
+  $('.grocery-input').val("");
 
 var checkbox = $('.checkbox');
 checkbox.on("click", remove);
 
   storePlan(cardContainer);
-};
+}
 
 function remove(){
   event.target.parentNode.remove();
